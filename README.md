da12032889c2a9f63ad8a14bade467a8d6937c21
Laird Nelson <laird.nelson@oracle.com>
2025-03-02 10:48:21 -0800

    Road grading; lays the groundwork for removing ScopeMember (#4)
    * Road grading; lays the groundwork for removing ScopeMember
    
    Signed-off-by: Laird Nelson <ljnelson@gmail.com>
    
    * Squashable commit; fixing Github actions problems
    
    Signed-off-by: Laird Nelson <ljnelson@gmail.com>
    
    ---------
    
    Signed-off-by: Laird Nelson <ljnelson@gmail.com>

--- README.md
+++ README.md
@@ -2,40 +2,35 @@
 
 [![Maven Central](https://maven-badges.herokuapp.com/maven-central/org.microbean/microbean-scope/badge.svg)](https://maven-badges.herokuapp.com/maven-central/org.microbean/microbean-scope)
 
-The microBean™ Scope project provides classes and interfaces assisting
-with implementing dependency injection scopes.
+The microBean™ Scope project provides classes and interfaces assisting with implementing dependency injection scopes.
 
 # Status
 
-This project is currently experimental, in a pre-alpha state, and
-unsuitable for production use.
+This project is currently experimental, in a pre-alpha state, and unsuitable for production use.
 
 # Compatibility
 
-**Until further notice, this project's APIs are subject to frequent
-backwards-incompatible signature and behavior changes, regardless of
-project version and without notice.**
+**Until further notice, this project's APIs are subject to frequent backwards-incompatible signature and behavior
+changes, regardless of project version and without notice.**
 
 # Requirements
 
-microBean™ Scope requires a Java runtime of version 17 or higher.
+microBean™ Scope requires a Java runtime of version 23 or higher.
 
 # Installation
 
-microBean™ Scope is available on [Maven
-Central](https://search.maven.org/).  Include microBean™ Scope as a
-Maven dependency:
+microBean™ Scope is available on [Maven Central](https://search.maven.org/).  Include microBean™ Scope as a Maven
+dependency:
 
 ```xml
 <dependency>
   <groupId>org.microbean</groupId>
   <artifactId>microbean-scope</artifactId>
   <!-- Always check https://search.maven.org/artifact/org.microbean/microbean-scope for up-to-date available versions. -->
-  <version>0.0.3</version>
+  <version>0.0.4</version>
 </dependency>
 ```
 
 # Documentation
 
-Full documentation is available at
-[microbean.github.io/microbean-scope](https://microbean.github.io/microbean-scope/).
+Full documentation is available at [microbean.github.io/microbean-scope](https://microbean.github.io/microbean-scope/).
--- src/main/java/org/microbean/scope/ScopeMember.java
+++ src/main/java/org/microbean/scope/ScopeMember.java
@@ -1,28 +1,50 @@
 /* -*- mode: Java; c-basic-offset: 2; indent-tabs-mode: nil; coding: utf-8-unix -*-
  *
- * Copyright © 2022–2023 microBean™.
+ * Copyright © 2022–2025 microBean™.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * the License. You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
 package org.microbean.scope;
 
 import java.util.Objects;
 
-import org.microbean.qualifier.NamedAttributeMap;
+import org.microbean.attributes.Attributes;
 
+/**
+ * A governed member of a governing {@link Scope}.
+ *
+ * @author <a href="https://about.me/lairdnelson" target="_top">Laird Nelson</a>
+ *
+ * @deprecated This interface will be removed with no replacement.
+ */
+@Deprecated(forRemoval = true)
 @FunctionalInterface
 public interface ScopeMember {
 
-  public NamedAttributeMap<?> governingScopeId();
+  /**
+   * Returns an {@link Attributes} identifying the scope that governs this {@link ScopeMember}.
+   *
+   * @return a non-{@code null} {@link Attributes}
+   */
+  public Attributes governingScopeId();
 
-  public default boolean governedBy(final NamedAttributeMap<?> governingScopeId) {
+  /**
+   * Returns {@code true} if and only if this {@link ScopeMember} is governed by a scope identified by the supplied
+   * {@code governingScopeId}.
+   *
+   * @param governingScopeId an {@link Attributes}; may be {@code null} in which case {@code false} will be returned
+   *
+   * @return {@code true} if and only if this {@link ScopeMember} is governed by a scope identified by the supplied
+   * {@code governingScopeId}
+   */
+  public default boolean governedBy(final Attributes governingScopeId) {
     return Objects.equals(this.governingScopeId(), governingScopeId);
   }
 
